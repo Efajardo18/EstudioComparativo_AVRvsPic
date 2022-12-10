4e55e55720b14d0c8b10d9c8433176ebef1f163c
Efajardo18 <78198673+Efajardo18@users.noreply.github.com>
2022-12-09 22:31:14 -0600

    22-12-09 SPI PT2
    Se agregaron los códigos referentes a SPI en PIC:
    -Maestro 
    -Esclavo
    -Librería SPI

---
+++ SPI/SPIPICSc.X/SPIcom.h
@@ -0,0 +1,154 @@
+/* 
+ * File:   SPIcom.h
+ * Author: Olifa
+ *
+ * Created on 23 de agosto de 2022, 04:47 PM
+ */
+
+#ifndef _XTAL_FREQ
+#define _XTAL_FREQ 8000000
+#endif	
+
+#define	XC_HEADER_TEMPLATE_H
+#define Master  1
+#define Slave   0
+#define End     1
+#define Mid     0
+#define _F4     0b00000000
+#define _F16    0b00000001
+#define _F64    0b00000010
+#define A       0
+#define B       1
+#define C       2
+#define D       3
+#define E       4
+
+#include <xc.h>
+
+void SPIPIC_set(char IntEnable, char EdgeSampled, int DataMode, char Role, char FrqSCK){
+    TRISC5 = 0;         //C5 es salida, SDO
+    if(Role == Master){
+        TRISC3 = 0;     //C3 es salida, SCK
+        if(EdgeSampled == End){
+            SSPSTAT |= (1<< _SSPSTAT_SMP_POSITION);
+        }
+        else if(EdgeSampled == Mid){
+            SSPSTAT &= ~(1<< _SSPSTAT_SMP_POSITION);
+        }
+        if(DataMode == A){
+            SSPSTAT &= ~(1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00100000|FrqSCK);
+        }
+        else if(DataMode == B){
+            SSPSTAT |= (1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00100000|FrqSCK);
+        }
+        else if(DataMode == C){
+            SSPSTAT &= ~(1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00110000|FrqSCK);
+        }
+        else if(DataMode == D){
+            SSPSTAT |= (1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00110000|FrqSCK);
+        }
+    }
+    else if(Role == Slave){
+        TRISC3 = 1;     //C3 es entrada, SCK recibe
+        if(DataMode == A){
+            SSPSTAT &= ~(1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00100100);
+        }
+        else if(DataMode == B){
+            SSPSTAT |= (1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00100100);
+        }
+        else if(DataMode == C){
+            SSPSTAT &= ~(1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00110100);
+        }
+        else if(DataMode == D){
+            SSPSTAT |= (1<< _SSPSTAT_CKE_POSITION);
+            SSPCON |= (0b00110100);
+        }
+    }
+    else{
+        
+    }
+    if (IntEnable == 1){
+        INTCON |= (1<< _INTCON_GIE_POSITION)|(1<< _INTCON_PEIE_POSITION);
+        PIE1 |= (1<< _PIE1_SSPIE_POSITION);
+    }
+    else if (IntEnable == 0){
+        PIE1 &= ~(1<< _PIE1_SSPIE_POSITION);
+    }
+}
+
+void SPIPIC_Rwait(void){
+    while(!SSPSTATbits.BF);
+}
+
+uint8_t SPIPIC_tnr(char Data){
+    SSPBUF  = Data;
+    SPIPIC_Rwait();
+    return SSPBUF;  
+}
+
+uint8_t SPIPIC_Rec(void){
+    SPIPIC_Rwait();
+    return SSPBUF;
+}
+
+unsigned SPIPIC_Busy(){
+    if(SSPSTATbits.BF){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+void SSstart(char Port, int SlaveNum){
+    if (Port == A)
+    {
+        PORTA &= ~(1<<SlaveNum);
+    }
+    else if (Port == B)
+    {
+        PORTB &= ~(1<<SlaveNum);
+    }
+    else if (Port == C)
+    {
+        PORTC &= ~(1<<SlaveNum);
+    }
+    else if (Port == D)
+    {
+        PORTD &= ~(1<<SlaveNum);
+    }
+    else if  (Port == E)
+    {
+        PORTE &= ~(1<<SlaveNum);
+    }
+}
+
+void SSstop(char Port, int SlaveNum){
+    if (Port == A)
+    {
+        PORTA |= (1<<SlaveNum);
+    }
+    else if (Port == B)
+    {
+        PORTB |= (1<<SlaveNum);
+    }
+    else if (Port == C)
+    {
+        PORTC |= (1<<SlaveNum);
+    }
+    else if (Port == D)
+    {
+        PORTD |= (1<<SlaveNum);
+    }
+    else if  (Port == E)
+    {
+        PORTE |= (1<<SlaveNum);
+    }
+}
---
+++ SPI/SPIPICSc.X/newmain.c
@@ -0,0 +1,13 @@
+/*
+ * File:   newmain.c
+ * Author: Olifa
+ *
+ * Created on 21 de octubre de 2022, 12:35 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
